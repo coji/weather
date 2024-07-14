f95b81651ee47880c480c3c8e758cb9d64189333
coji <coji@techtalk.jp>
2024-07-14 23:05:04 +0900

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by coji on 2024/07/14.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
